2f090be08fe170bbca82e8712e93c6cc42706b7a
jesse <nitchsx@gmail.com>
2025-03-12 03:38:49 -0300

    feat: little project of spring web and rest

---
+++ apiRest/src/main/java/api/apirest/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package api.apirest.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @GetMapping
+    public String welcome() {
+        return "Welcome to the API REST";
+    }
+}
