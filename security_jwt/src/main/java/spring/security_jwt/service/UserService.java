c73fe3a9233f359aa87ebf41f09a6ea357781a84
jesse <nitchsx@gmail.com>
2025-03-15 00:23:31 -0300

    feat: class of jwt

---
+++ security_jwt/src/main/java/spring/security_jwt/service/UserService.java
@@ -0,0 +1,22 @@
+package spring.security_jwt.service;
+
+
+import spring.security_jwt.model.User;
+import spring.security_jwt.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+    @Autowired
+    private PasswordEncoder encoder;
+    public void createUser(User user){
+        String pass = user.getPassword();
+        //criptografando antes de salvar no banco
+        user.setPassword(encoder.encode(pass));
+        repository.save(user);
+    }
+}
